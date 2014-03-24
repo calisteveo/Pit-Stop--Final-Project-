67049581e894139229338b11163aee6d08731027
calisteveo <soglesbee1@gmail.com>
2014-03-24 11:58:42 -0700

    added controllers for runs and run details previous commit added routing

---
+++ app/assets/javascripts/controllers/main/runCtrl.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var runControllers = angular.module('runControllers', []);
+
+runControllers.controller('RunListCtrl', ['$scope',
+  function($scope){
+    $scope.runs = data;
+  }]);
+
+runControllers.controller('RunDetailCtrl', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+    $scope.runId = $routeParams.runId;
+  }]);
